8acb641a477ece2831de740bf68a1b103b1b1472
bhuvibhatnagar <bhatnagarbhuvi24@gmail.com>
2026-09-04 23:18:03 +0530

    Allow Vercel frontend in CORS

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:5173",
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://payshield-tau.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
